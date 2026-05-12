8e3f6e420fb4bed97cae06621caf7d45fa386abf
Jiří Vrba <jiri@vrba.dev>
2026-05-12 17:07:16 +0200

    Use kitten to ssh connect

--- terraform/connect.sh
+++ terraform/connect.sh
@@ -7,4 +7,4 @@ if [ ! -f ./hetzner-ssh-key ]; then
     exit 1
 fi
 
-ssh -4 -i ./hetzner-ssh-key -o "StrictHostKeyChecking no" root@$(tofu output -raw public_ip)
+kitten ssh -4 -i ./hetzner-ssh-key -o "StrictHostKeyChecking no" root@$(tofu output -raw public_ip)
